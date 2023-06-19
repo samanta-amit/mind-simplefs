aef8030624474936f4b1094c9a9c0b3633e88ee4
Ryan Stutsman <stutsman@cs.utah.edu>
2023-06-19 19:05:45 +0000

    Cleanup of simplefs_readpage().
    - Refactored MIND-related parts into mind_page_fetch().
    - Eliminated dead code.
    - Tried to clean up some logging without removing it all.
    - Fixed compiler warnings.
      - Fixed format string bugs.
      - Fixed mixed declarations warnings.

--- fs.c
+++ fs.c
@@ -19,9 +19,6 @@
 #include "simplefs.h"
 
 
-/* spin locks for hashtable */
-struct spinlock *test_spin_lock;
-
 unsigned long sharedaddress;
 unsigned long shmem_address[10];
 static int readAddress = 0;
@@ -29,8 +26,6 @@ static int readAddress = 0;
 //https://tldp.org/LDP/lkmpg/2.4/html/x354.htm (also used this for printing longs)
 module_param(readAddress,int, 0);
 module_param_array(shmem_address, long, NULL, 0);
-
-spinlock_t pgfault_lock;
 
 /* Mount a simplefs partition */
 struct dentry *simplefs_mount(struct file_system_type *fs_type,
@@ -68,19 +63,18 @@ static struct file_system_type simplefs_file_system_type = {
 
 static int __init simplefs_init(void)
 {
-    pr_info("loading simplefs\n");
-    unsigned long mm_addr;
     int i;
-    struct task_struct *tsk;
-    pr_info("value of readAddress %d", readAddress);
+    int ret;
     u64 alloc_size = sizeof(3 * PAGE_SIZE);
-    sharedaddress = 18446718784707231744;   //-234881024; //alloc_kshmem(alloc_size, DISAGG_KSHMEM_SERV_FS_ID);
 
+    pr_info("loading simplefs\n");
+    pr_info("value of readAddress %d", readAddress);
+    sharedaddress = 18446718784707231744llu;   //-234881024; //alloc_kshmem(alloc_size, DISAGG_KSHMEM_SERV_FS_ID);
 
     if(!readAddress){
 	    pr_info("addresses:");
 	    for(i = 0; i < 10; i++){
-		    shmem_address[i] = alloc_kshmem(alloc_size, DISAGG_KSHMEM_SERV_FS_ID);
+		    shmem_address[i] = (uintptr_t)alloc_kshmem(alloc_size, DISAGG_KSHMEM_SERV_FS_ID);
 		    pr_info("%ld, ", shmem_address[i]);
 	    }
 		pr_info("single print addresses %ld, %ld, %ld, %ld, %ld, %ld, %ld, %ld, %ld, %ld", shmem_address[0],
@@ -116,17 +110,7 @@ static int __init simplefs_init(void)
 	    pr_info("KernShmem: alloc with VA result Requested [0x%lx] <-> Received [0x%lx]\n", test_address, temp);
     }*/
 
-   
-
-
-   test_spin_lock = kmalloc(sizeof(struct spinlock), GFP_KERNEL);
-//   pgfault_lock = kmalloc(sizeof(struct spinlock), GFP_KERNEL);
-   spin_lock_init(&pgfault_lock);
-
-   /* initializes the spin lock */
-    spin_lock_init(test_spin_lock);
-
-    int ret = simplefs_init_inode_cache();
+    ret = simplefs_init_inode_cache();
     if (ret) {
         pr_err("inode cache creation failed\n");
         goto end;
