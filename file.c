8810af10b2402deacfddfcc5be3d6d571a535a48
Amit Samanta <amit.samanta049@gmail.com>
2023-06-21 23:24:41 +0000

    Moved generic_file_buffered_read to simplefs module + working version of mutiple pages

--- file.c
+++ file.c
@@ -25,6 +25,7 @@
 #include <../../roce_modules/roce_for_disagg/roce_disagg.h>
 #include <asm/traps.h>
 #include <../include/disagg/kshmem_disagg.h>
+#include <linux/swap.h> /* for the mark_page_accessed function*/
 
 #include "simplefs.h"
 
@@ -958,6 +959,321 @@ ssize_t simplefs_kernel_read(struct file *file, void *buf, size_t count, loff_t 
 
 }
 
+/**
+ * generic_file_buffered_read - generic file read routine
+ * @iocb:       the iocb to read
+ * @iter:       data destination
+ * @written:    already copied
+ *
+ * This is a generic file read routine, and uses the
+ * mapping->a_ops->readpage() function for the actual low-level stuff.
+ *
+ * This is really ugly. But the goto's actually try to clarify some
+ * of the logic when it comes to error handling etc.
+ */
+static ssize_t simplefs_generic_file_buffered_read(struct kiocb *iocb,
+                struct iov_iter *iter, ssize_t written)
+{
+        pr_info("inside simplefs_generic_file_buffered_read\n");
+        struct file *filp = iocb->ki_filp;
+        struct address_space *mapping = filp->f_mapping;
+        struct inode *inode = mapping->host;
+        struct file_ra_state *ra = &filp->f_ra;
+        loff_t *ppos = &iocb->ki_pos;
+        pgoff_t index;
+        pgoff_t last_index;
+        pgoff_t prev_index;
+        unsigned long offset;      /* offset into pagecache page */
+        unsigned int prev_offset;
+        int error = 0;
+
+        if (unlikely(*ppos >= inode->i_sb->s_maxbytes))
+                return 0;
+        iov_iter_truncate(iter, inode->i_sb->s_maxbytes);
+
+        index = *ppos >> PAGE_SHIFT;
+        prev_index = ra->prev_pos >> PAGE_SHIFT;
+        prev_offset = ra->prev_pos & (PAGE_SIZE-1);
+        last_index = (*ppos + iter->count + PAGE_SIZE-1) >> PAGE_SHIFT;
+        offset = *ppos & ~PAGE_MASK; //offset into the page
+
+        for (;;) {
+                struct page *page;
+                pr_info("looping page index %d offset %d\n", index, offset);
+                pgoff_t end_index;
+                loff_t isize;
+                unsigned long nr, ret;
+
+                cond_resched();
+find_page:
+                pr_info("find page\n");
+
+                page = find_get_page(mapping, index);
+                ClearPageUptodate(page);
+
+
+                if (!page) {
+                        pr_info("no page found from findgetpage\n");
+                        if (iocb->ki_flags & IOCB_NOWAIT)
+                                goto would_block;
+                        page_cache_sync_readahead(mapping,
+                                        ra, filp,
+                                        index, last_index - index);
+                        page = find_get_page(mapping, index);
+                        if (unlikely(page == NULL))
+                                goto no_cached_page;
+                }
+                pr_info("page found is %d\n", page);
+                if (PageReadahead(page)) {
+                        page_cache_async_readahead(mapping,
+                                        ra, filp, page,
+                                        index, last_index - index);
+                        pr_info("sync readahead\n");
+
+                }
+                if (!PageUptodate(page)) {
+                        pr_info("page not up to date\n");
+                        if (iocb->ki_flags & IOCB_NOWAIT) {
+                                put_page(page);
+                                goto would_block;
+                        }
+
+                        /*
+                         * See comment in do_read_cache_page on why
+                         * wait_on_page_locked is used to avoid unnecessarily
+                         * serialisations and why it's safe.
+                         */
+                        error = wait_on_page_locked_killable(page);
+                        if (unlikely(error))
+                                goto readpage_error;
+                        if (PageUptodate(page))
+                                goto page_ok;
+
+                        if (inode->i_blkbits == PAGE_SHIFT ||
+                                        !mapping->a_ops->is_partially_uptodate)
+                                goto page_not_up_to_date;
+                        /* pipes can't handle partially uptodate pages */
+                        if (unlikely(iter->type & ITER_PIPE))
+                                goto page_not_up_to_date;
+                        if (!trylock_page(page))
+                                goto page_not_up_to_date;
+                        /* Did it get truncated before we got the lock? */
+                        if (!page->mapping)
+                                goto page_not_up_to_date_locked;
+                        if (!mapping->a_ops->is_partially_uptodate(page,
+                                                        offset, iter->count))
+                                goto page_not_up_to_date_locked;
+                        unlock_page(page);
+                }
+                pr_info("page was marked up to date\n");
+page_ok:
+                pr_info("page_ok \n");
+                /*
+                 * i_size must be checked after we know the page is Uptodate.
+                 *
+                 * Checking i_size after the check allows us to calculate
+                 * the correct value for "nr", which means the zero-filled
+                 * part of the page is not copied back to userspace (unless
+                 * another truncate extends the file - this is desired though).
+                 */
+
+                isize = i_size_read(inode);
+                end_index = (isize - 1) >> PAGE_SHIFT;
+                if (unlikely(!isize || index > end_index)) {
+                        pr_info("size difference\n");
+                        put_page(page);
+                        goto out;
+                }
+
+                /* nr is the maximum number of bytes to copy from this page */
+                nr = PAGE_SIZE;
+                if (index == end_index) {  //current page and last page
+
+                        nr = ((isize - 1) & ~PAGE_MASK) + 1; //inode size (8kb? - 1)
+                        if (nr <= offset) { //if nr <= offset into page
+                                pr_info("offset not right nr was %d offset %d isize %d\n", nr, offset, isize);
+                                put_page(page);
+                                goto out;
+                        }
+                }
+                nr = nr - offset;
+
+                /* If users can be writing to this page using arbitrary
+                 * virtual addresses, take care about potential aliasing
+                 * before reading the page on the kernel side.
+                 */
+                if (mapping_writably_mapped(mapping))
+                        flush_dcache_page(page);
+
+                /*
+                 * When a sequential read accesses a page several times,
+                 * only mark it as accessed the first time.
+                 */
+                if (prev_index != index || offset != prev_offset)
+                        mark_page_accessed(page);
+                prev_index = index;
+
+                /*
+                 * Ok, we have the page, and it's up-to-date, so
+                 * now we can copy it to user space...
+                 */
+                pr_info("data being copied to page here \n");
+                ret = copy_page_to_iter(page, offset, nr, iter);
+                offset += ret;
+                index += offset >> PAGE_SHIFT;
+                offset &= ~PAGE_MASK;
+                prev_offset = offset;
+
+                put_page(page);
+                written += ret;
+                if (!iov_iter_count(iter))
+                        goto out;
+                if (ret < nr) {
+                        error = -EFAULT;
+                        goto out;
+                }
+                continue;
+
+page_not_up_to_date:
+                pr_info("page not up to date\n");
+                /* Get exclusive access to the page ... */
+                error = lock_page_killable(page);
+                if (unlikely(error))
+                        goto readpage_error;
+
+page_not_up_to_date_locked:
+                pr_info("page not up to date locked\n");
+                /* Did it get truncated before we got the lock? */
+                if (!page->mapping) {
+                        unlock_page(page);
+                        put_page(page);
+                        continue;
+                }
+
+                /* Did somebody else fill it already? */
+                if (PageUptodate(page)) {
+                        unlock_page(page);
+                        goto page_ok;
+                }
+
+readpage:
+                /*
+                 * A previous I/O error may have been due to temporary
+                 * failures, eg. multipath errors.
+                 * PG_error will be set again if readpage fails.
+                 */
+                ClearPageError(page);
+                /* Start the actual read. The read will unlock the page. */
+                error = mapping->a_ops->readpage(filp, page);
+                pr_info("after readpage called \n");
+                pr_info("going to page_ok\n");
+                goto page_ok;
+
+readpage_error:
+                pr_info("read page error \n");
+                /* UHHUH! A synchronous read error occurred. Report it */
+                put_page(page);
+                goto out;
+
+no_cached_page:
+                pr_info("no cached page \n");
+                /*
+                 * Ok, it wasn't cached, so we need to create a new
+                 * page..
+                 */
+                page = page_cache_alloc(mapping);
+                if (!page) {
+                        error = -ENOMEM;
+                        goto out;
+                }
+                error = add_to_page_cache_lru(page, mapping, index,
+                                mapping_gfp_constraint(mapping, GFP_KERNEL));
+                if (error) {
+                        put_page(page);
+                        if (error == -EEXIST) {
+                                error = 0;
+                                goto find_page;
+                        }
+                        goto out;
+                }
+                goto readpage;
+        }
+
+would_block:
+        pr_info("would_block\n");
+        error = -EAGAIN;
+out:
+        ra->prev_pos = prev_index;
+        ra->prev_pos <<= PAGE_SHIFT;
+        ra->prev_pos |= prev_offset;
+
+        *ppos = ((loff_t)index << PAGE_SHIFT) + offset;
+        file_accessed(filp);
+        pr_info("leaving generic_file_buffered_read\n");
+        return written ? written : error;
+}
+
+
+
+
+ssize_t
+simplefs_generic_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
+{
+        size_t count = iov_iter_count(iter);
+        ssize_t retval = 0;
+
+        if (!count)
+                goto out; /* skip atime */
+
+        if (iocb->ki_flags & IOCB_DIRECT) {
+                struct file *file = iocb->ki_filp;
+                struct address_space *mapping = file->f_mapping;
+                struct inode *inode = mapping->host;
+                loff_t size;
+
+                size = i_size_read(inode);
+                if (iocb->ki_flags & IOCB_NOWAIT) {
+                        if (filemap_range_has_page(mapping, iocb->ki_pos,
+                                                   iocb->ki_pos + count - 1))
+                                return -EAGAIN;
+                } else {
+                        retval = filemap_write_and_wait_range(mapping,
+                                                iocb->ki_pos,
+                                                iocb->ki_pos + count - 1);
+                        if (retval < 0)
+                                goto out;
+                }
+
+                file_accessed(file);
+
+                retval = mapping->a_ops->direct_IO(iocb, iter);
+                if (retval >= 0) {
+                        iocb->ki_pos += retval;
+                        count -= retval;
+                }
+                iov_iter_revert(iter, count - iov_iter_count(iter));
+
+                /*
+                 * Btrfs can have a short DIO read if we encounter
+                 * compressed extents, so if there was an error, or if
+                 * we've already read everything we wanted to, or if
+                 * there was a short read because we hit EOF, go ahead
+                 * and return.  Otherwise fallthrough to buffered io for
+                 * the rest of the read.  Buffered reads will not work for
+                 * DAX files, so don't bother trying.
+                 */
+                if (retval < 0 || !count || iocb->ki_pos >= size ||
+                    IS_DAX(inode))
+                        goto out;
+        }
+
+        retval = simplefs_generic_file_buffered_read(iocb, iter, retval);
+out:
+        return retval;
+}
+
+
+
 ssize_t
 simplefs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 {
@@ -967,7 +1283,7 @@ simplefs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 	struct file *file = iocb->ki_filp;
 	struct address_space *mapping = file->f_mapping;
 	struct inode *inode = mapping->host;
-
+	int pageindex;
 
 
 	pr_info("read ki_pos %d", iocb->ki_pos);
@@ -1000,9 +1316,17 @@ simplefs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 	//invalidating the page
 	callinvalidatepage(inode->i_ino, index, 1);
 
+	for(pageindex = index; pageindex <= last_index; pageindex++){
+                pr_info("page index attempting to invalidate is %d\n", pageindex);
+                struct page *page = find_get_page(mapping, index);
+                pr_info("getting page pointer in file read iter %d\n", page);
+                if(page != 0){
+                        ClearPageUptodate(page);
+                        pr_info("marking page %d out of date\n", pageindex);
+                }
+        }
 
-
-		retval = generic_file_read_iter(iocb, iter);
+	retval = simplefs_generic_file_read_iter(iocb, iter);
 
    	pr_info("**** reading into vector type %d, length %d ki_flags %d ki_hint %d nr_segs %d ki_pos %d", iter->type, (iter->iov)->iov_len, iocb->ki_flags, iocb->ki_flags, iocb->ki_hint, iter->nr_segs, iocb->ki_pos);
 
@@ -1019,11 +1343,6 @@ simplefs_file_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 	inode_unlock(inode);
 	/*      ~*~       */
 	pr_info("****ending read");
-
-
-
-
-
 	return retval;
 
 }
